5da560f85e5ef6dd6207c35e385c7f642dcb6289
ivanng01 <ivann-gonzalez@hotmail.com>
2023-09-27 09:26:38 -0300

    ejemplo dapper

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using DapperE.Entidades;
+using DapperE.Servicios;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+
+Console.WriteLine("Listado del Registro Usuarios");
+var p1 = UsuariosCons.GetListado();
+ foreach (var p in p1)
+ {
+     Console.WriteLine($"{p.Id} {p.Nombre} {p.Edad}");
+ }
+
+Console.WriteLine("**************************");
+
+Console.WriteLine("Buscar Registro por ID");
+var personaId = UsuariosCons.GetId(3);
+    Console.WriteLine($"El registro se llama {personaId.Nombre} tiene {personaId.Edad} años");
+
+Console.ReadKey();
+
---
+++ Servicios/UsuariosCons.cs
@@ -0,0 +1,44 @@
+﻿using DapperE.Entidades;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+
+namespace DapperE.Servicios
+{
+    public class UsuariosCons
+    {
+        private static string _conect = @"Data Source=DESKTOP-LD9U44S\SQLEXPRESS;Initial Catalog=Usuarios;Integrated Security=True; TrustServerCertificate=True";
+        public static List<Usuarios> GetListado()
+        {
+            string sql = @"Select * from users 
+                          Order by Nombre";
+            //Inicio BD
+            var db = new SqlConnection(_conect);
+
+            //Ejecuto consulta SQL y almaceno las líneas en modelo. 
+            var p1 = db.Query<Usuarios>(sql).ToList();
+
+            return p1;
+        }
+
+        public static Usuarios GetId(int Id)
+        {
+            string sql = @"Select * from users  
+                          Where Id = @id";
+
+            var db = new SqlConnection(_conect);
+
+            //Agrego first para que devuelva una sola línea
+            var persid = db.QueryFirst<Usuarios>(sql, new { Id });
+
+            //Devuelvo el objeto.
+            return persid;
+        }
+    }
+}
+
